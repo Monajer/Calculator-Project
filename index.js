67e098155c5e99d08014df1d6cbf3a920aa63e19
monajer <rabbiyusuf003@gmail.com>
2023-12-25 02:46:59 +0600

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-function rabbi(){
-    var myCal = document.getElementById("display").value; 
-    var y = math.evaluate(myCal);
-    document.getElementById("display").value = y;
+    // function rabbi(){
+    //     var myCal = document.getElementById("display").value; 
+    //     var y = math.evaluate(myCal);
+    //     document.getElementById("display").value = y;
+        
+    // }
+
+    let nam = prompt("Enter Your Names First Letter: ");
+    let finNam = nam.toUpperCase();
+    console.log(finNam);
     
-}
+    switch (finNam) {
+        case 'R':
+            console.log("Rabbi Yusuf");
+            break;
+        case 'A':
+            console.log("Carbon Anik");
+            break;    
+        case 'M':
+            console.log("Monajer Hakim Munna");
+            break;
+        default:
+            console.log("Sorry :) ");
+    }
